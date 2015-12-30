1c2a10c7cfa69aacffb3be6efffd2e255905bd2a
qianfeng <qianfeng@LMD.local>
2015-12-30 14:51:33 +0800

    update AppDelegate &&add image file

---
+++ HGNewsDemo/SBText.h
@@ -0,0 +1,13 @@
+//
+//  SBText.h
+//  HGNewsDemo
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 何光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBText : UIView
+
+@end
---
+++ HGNewsDemo/SBText.m
@@ -0,0 +1,27 @@
+//
+//  SBText.m
+//  HGNewsDemo
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 何光. All rights reserved.
+//
+
+#import "SBText.h"
+
+@implementation SBText
+
+
+- (void)drawRect:(CGRect){
+    
+    和光是男优
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
